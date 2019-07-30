db0970e1b8dfe620a6eec2d865ceceaf37008caf
nutmeg-5000 <z5208738@student.unsw.edu.au>
2019-07-31 00:41:53 +1000

    fourth

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1564449389",
+        "cacheTag": "1564497482",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1564448792,
+            "last_rendertime": 1564496853,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
